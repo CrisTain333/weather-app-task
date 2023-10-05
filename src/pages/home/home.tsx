8006994fc577112f874366e67371291da21f58c0
Sukanta Das <arnobh84@gmail.com>
2023-10-05 12:11:25 +0600

    navbar component added

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,28 +1,8 @@
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Cloud } from "lucide-react";
+import Navbar from "@/components/Navbar/Navbar";
 const Home = () => {
   return (
     <div>
-      <nav>
-        {/* left side */}
-        <div className="flex justify-between my-2">
-          <div className="flex bg-gray-100 items-center px-2 py-3 rounded-md">
-            {" "}
-            <Cloud color="black" />{" "}
-            <span className="ml-2 font-bold">Weather</span>
-          </div>
-
-          <div className="flex items-center">
-            <Input
-              placeholder="Enter city"
-              className="w-[70vw]"
-            />
-
-            <Button className="ml-3">Search</Button>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
     </div>
   );
 };
