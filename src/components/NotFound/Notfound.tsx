292432659783d742c112a5947a74f021a5580b0d
Sukanta Das <arnobh84@gmail.com>
2023-10-05 16:26:19 +0600

    new component added

---
+++ src/components/NotFound/Notfound.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Notfound = () => {
+  return (
+    <div>
+      <div className="mt-20 space-y-3">
+        <h2 className="text-5xl text-center font-semibold">
+          City not found
+        </h2>
+        <p className="text-2xl text-center font-semibold">
+          Please enter a valid city name
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Notfound;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar/Navbar";
+import Notfound from "@/components/NotFound/Notfound";
 import Overview from "@/components/Overview/Overview";
 import { useAppSelector } from "@/redux/hook";
 
@@ -9,18 +10,39 @@ const Home = () => {
   console.log("Data", data);
   console.log("isLoading", isDataLoading);
   console.log("CityNot Found", isCityNotFound);
+
+  // if (!data && isCityNotFound === false) {
+  //   <div className="mt-20 space-y-3">
+  //     <h2 className="text-5xl text-center font-semibold">
+  //       Welcome to the Weather App
+  //     </h2>
+  //     <p className="text-2xl text-center font-semibold">
+  //       Enter a city name to get the weather forecast
+  //     </p>
+  //   </div>;
+
+  //   return;
+  // }
   return (
     <div>
       <Navbar />
 
-      {/* Main Content */}
-      <main>
-        <h1 className="text-3xl font-medium my-8">
-          Today Overview
-        </h1>
+      {!isDataLoading && isCityNotFound ? (
+        <>
+          <Notfound />
+        </>
+      ) : (
+        <>
+          {/* Main Content */}
+          <main>
+            <h1 className="text-3xl font-medium my-8">
+              Today Overview
+            </h1>
 
-        <Overview data={data} />
-      </main>
+            <Overview data={data} />
+          </main>
+        </>
+      )}
     </div>
   );
 };
